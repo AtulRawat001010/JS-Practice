cdb2887a65f779f8f476b7c442d9fc90a3adb845
AtulRawat <workerhere12@gmail.com>
2023-10-12 00:22:06 +0530

    #

--- index3.js
+++ index3.js
@@ -3,11 +3,13 @@ let stories = document.querySelector(".stories");
 
 let arr = [
     {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
-     story:"https://rukminim2.flixcart.com/image/416/416/xif0q/keyboard/gaming-keyboard/r/g/v/gaming-keyboard-and-mouse-combo-keyboard-with-7-color-backlit-original-imaggjk5ahg9jzny.jpeg?q=70"},
+    story:"https://www.bing.com/th/id/OIP.K7UO5CKAbibMwLhGpYRVKQHaEK?w=323&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7"},
+
     {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
      story:"https://rukminim2.flixcart.com/image/416/416/xif0q/keyboard/gaming-keyboard/r/g/v/gaming-keyboard-and-mouse-combo-keyboard-with-7-color-backlit-original-imaggjk5ahg9jzny.jpeg?q=70"},
+
     {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
-     story:"https://rukminim2.flixcart.com/image/416/416/xif0q/keyboard/gaming-keyboard/r/g/v/gaming-keyboard-and-mouse-combo-keyboard-with-7-color-backlit-original-imaggjk5ahg9jzny.jpeg?q=70"}
+    story:"https://rukminim2.flixcart.com/image/416/416/xif0q/keyboard/gaming-keyboard/r/g/v/gaming-keyboard-and-mouse-combo-keyboard-with-7-color-backlit-original-imaggjk5ahg9jzny.jpeg?q=70"}
 ];
 
 let clut = "";
@@ -21,5 +23,12 @@ arr.forEach((element, index) => {
 stories.innerHTML = clut;
 
 stories.addEventListener("click", (e)=> {
+    let showStory = document.querySelector(".showStory");
     console.log(e.target.id);
+    console.log(arr[e.target.id].story);
+    showStory.style.backgroundImage = `url(${arr[e.target.id].story})`;
+    showStory.style.display = `block`;
+    setTimeout(() => {
+        showStory.style.display = "none";
+    }, 2000);
 })
