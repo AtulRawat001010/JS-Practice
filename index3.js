a29c129e3c8105e365244523c56cc7cf190e4ee0
AtulRawat <workerhere12@gmail.com>
2023-10-12 23:19:36 +0530

    #

--- index3.js
+++ index3.js
@@ -9,7 +9,16 @@ let arr = [
      story:"https://rukminim2.flixcart.com/image/416/416/xif0q/keyboard/gaming-keyboard/r/g/v/gaming-keyboard-and-mouse-combo-keyboard-with-7-color-backlit-original-imaggjk5ahg9jzny.jpeg?q=70"},
 
     {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
-    story:"https://rukminim2.flixcart.com/image/416/416/xif0q/keyboard/gaming-keyboard/r/g/v/gaming-keyboard-and-mouse-combo-keyboard-with-7-color-backlit-original-imaggjk5ahg9jzny.jpeg?q=70"}
+    story:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70"},
+    {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
+    story:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70"},{dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
+    story:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70"},
+    // {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
+    // story:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70"},
+    // {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
+    // story:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70"},
+    // {dp:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70",
+    // story:"https://rukminim2.flixcart.com/image/312/312/xif0q/keyboard/gaming-keyboard/f/p/k/it-kb331-intex-original-imagmgwzzxn2m3pu.jpeg?q=70"}
 ];
 
 let clut = "";
