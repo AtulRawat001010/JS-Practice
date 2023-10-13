114e08085deef003466124608586712adda8d226
AtulRawat <workerhere12@gmail.com>
2023-10-14 00:55:10 +0530

    #

---
+++ quiz.js
@@ -0,0 +1,50 @@
+const quizData = [
+    {
+        Que: "how Old Is Javascript?",
+        a:"10+ Year",
+        b:"20+ Year",
+        c:"30+ Year",
+        d:"40+ Year",
+        correct:"b"
+    },
+    {
+        Que: "What does Dom means?",
+        a:"Document Object Model",
+        b:"Data Orient Manager",
+        c:"Document Object Module",
+        d:"Document Object Method",
+        correct:"a"
+    },
+    {
+        Que: "How Old Is Ecma6 is?",
+        a:"2016",
+        b:"2010",
+        c:"2015",
+        d:"2017",
+        correct:"c"
+    },
+    {
+        Que: "What is the characterstic of Javascript?",
+        a:"Single Threded Language",
+        b:"Dynamiclly Typed Language",
+        c:"Server Side Language",
+        d:"All Of The Above",
+        correct:"d"
+    },
+    {
+        Que: "Reason, how is Js different from Java?",
+        a:"It is Different ik.",
+        b:"Its same.",
+        c:"Js is java with scripts",
+        d:"All Of The Above",
+        correct:"a"
+    }
+]
+
+let currentQuetion = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    
+}
