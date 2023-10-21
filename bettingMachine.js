71e0c4fbdd9e6a2d0cecf4ce907de83539bb1bbb
AtulRawat <workerhere12@gmail.com>
2023-10-22 00:03:53 +0530

    #

---
+++ bettingMachine.js
@@ -0,0 +1,20 @@
+// 1. deposit money.
+// 2. get number of line to bet on.
+// 3. collect bet amount.
+// 4.spin the slot money.
+// 5.check if the user won.
+// 6. give user winning.
+// 7. play again.
+
+const prompt = require("prompt-sync")();
+
+main();
+
+const deposit = ()=> {
+    const depositAmount = prompt("Enter a deposit amount: ");
+}
+
+
+function main() {
+    deposit();
+}
