303ab510e9f6a4313e69b28a60af4d06516a86db
AtulRawat <workerhere12@gmail.com>
2023-10-31 02:43:28 +0530

    #

--- toDoApp.js
+++ toDoApp.js
@@ -1,21 +1,160 @@
-const input = document.querySelector("#input");
+// const input = document.querySelector("#input");
+// const form = document.querySelector("#form");
+// const tasks = document.querySelector("#tasks");
+// const supUl = document.querySelector("#supUl");
+// const sup = document.querySelector(".sup");
+// const todosTasks = JSON.parse(localStorage.getItem("todosTasks"));
 
-const form = document.querySelector("#form");
+// if (todosTasks) {
+//     todosTasks.forEach((todoTask) => {
+//         addTask(todoTask);
+//     });
+// }
 
-const tasks = document.querySelector("#tasks");
 
-form.addEventListener("submit", (e)=> {
+// sup.addEventListener("click", () => {
+//     supUl.classList.toggle("hiddenUl");
+// });
+
+// form.addEventListener("submit", (e)=> {
+//     e.preventDefault();
+
+//     addTask();
+// });
+
+
+// function addTask(todoTask) {
+//     let todoText = input.value;
+
+//     if(todoTask){
+//         todoText = todoTask.text;
+//     }
+
+//     if (todoText) {
+//         const taskEl = document.createElement("li");
+//         taskEl.classList.add("list");
+
+//         if (todoTask && todoTask.completed) {
+//             taskEl.classList.add("completed");
+//         }
+
+//         taskEl.innerText = todoText;
+
+//         taskEl.addEventListener("click", ()=> {
+//             taskEl.classList.toggle("completed");
+
+//             updateLS();
+//         });
+
+//         taskEl.addEventListener("contextmenu", (e)=> {
+//             e.preventDefault();
+//             taskEl.remove();
+
+//             updateLS();
+//         });
+
+//         tasks.appendChild(taskEl);
+//         input.value = "";
+//     }
+
+//     updateLS();
+// }
+
+
+// function updateLS() {
+//     const tasksEl = document.querySelectorAll(".list");
+
+//     const tasks = [];
+
+//     tasksEl.forEach(taskEl => {
+//         tasks.push({
+//             text: taskEl.innerText,
+//             completed: taskEl.classList.contains("completed")
+//         });
+//     });
+
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+// }
+
+
+
+
+
+
+
+const supUl = document.querySelector("#supUl");
+const sup = document.querySelector(".sup");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("tasks");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+sup.addEventListener("click", () => {
+    supUl.classList.toggle("hiddenUl");
+});
+
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const todoText = input.value;
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
+    }
 
     if (todoText) {
-        const taskEl = document.createElement("li");
+        const todoEl = document.createElement("li");
+        todoEl.classList.add("list");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
 
-        taskEl.innerText = todoText;
+        todoEl.innerText = todoText;
 
-        tasks.appendChild(taskEl);
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
 
         input.value = "";
+
+        updateLS();
     }
-});
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll(".list");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
