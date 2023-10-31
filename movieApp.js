94198c94a4f8403011ace40b26b8fb46dd953005
AtulRawat <workerhere12@gmail.com>
2023-11-01 02:15:23 +0530

    #

---
+++ movieApp.js
@@ -0,0 +1,45 @@
+const APIKEY = "04c35731a5ee918f014970082a0088b1";
+const APIURL =
+    `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}&page=1`;
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.querySelector('#main');
+
+async function getMovie() {
+    const res = await fetch(APIURL);
+    const data = await res.json()
+    console.log(data);
+
+    data.results.forEach(movie => {
+        const { title, poster_path, vote_average } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+        <img src="${IMGPATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        `
+
+        main.appendChild(movieEl);
+    });
+
+    return data;
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    }else if (vote >= 5) {
+        return "orange";
+    }else{
+        return "red";
+    }
+}
+
+getMovie();
