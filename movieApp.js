0b218acc323ee66086e70e1e9ac64e5f2227d851
AtulRawat <workerhere12@gmail.com>
2023-11-01 03:29:51 +0530

    #

--- movieApp.js
+++ movieApp.js
@@ -1,9 +1,11 @@
 const APIKEY = "04c35731a5ee918f014970082a0088b1";
-const APIURL =
-    `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}&page=1`;
+
+const APIURL =`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}&page=1`;
+
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 
 const main = document.querySelector('#main');
 const form = document.querySelector('#form');
@@ -15,7 +17,7 @@ getMovie(APIURL);
 async function getMovie(url) {
     const res = await fetch(url);
     const data = await res.json()
-    console.log(data);
+    // console.log(data);
 
     data.results.forEach(movie => {
         const { title, poster_path, vote_average } = movie;
