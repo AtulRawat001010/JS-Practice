644713b83fc68640d087bd28221d546d4e2ce4b5
AtulRawat <workerhere12@gmail.com>
2023-11-01 03:18:28 +0530

    #

--- movieApp.js
+++ movieApp.js
@@ -6,9 +6,14 @@ const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main = document.querySelector('#main');
+const form = document.querySelector('#form');
+const search = document.querySelector('#search');
 
-async function getMovie() {
-    const res = await fetch(APIURL);
+
+getMovie(APIURL);
+
+async function getMovie(url) {
+    const res = await fetch(url);
     const data = await res.json()
     console.log(data);
 
@@ -29,7 +34,32 @@ async function getMovie() {
         main.appendChild(movieEl);
     });
 
-    return data;
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+
+    main.innerHTML = "";
+
+    movies.forEach(movie => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+        <img src="${IMGPATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class = "overview">
+        <h4>Overview:</h4>
+        ${overview}</div>
+        `
+
+        main.appendChild(movieEl);
+    });
 }
 
 function getClassByRate(vote) {
@@ -42,4 +72,14 @@ function getClassByRate(vote) {
     }
 }
 
-getMovie();
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm){
+        getMovie(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+})
