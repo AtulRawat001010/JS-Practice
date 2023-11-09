47ac4ba0ec7000061fc92875a7732d911b0eb9ff
AtulRawat <workerhere12@gmail.com>
2023-11-09 20:36:35 +0530

    #

--- githubProfileApp.js
+++ githubProfileApp.js
@@ -1,39 +1,85 @@
 const APIURL = "https://api.github.com/users/";
 
 const main = document.querySelector('#main');
+const form = document.querySelector('#form');
+const search = document.querySelector('#search');
 
 
-getUser(user = "atulrawat001010");
+getUser(username = "atulrawat001010");
 
-async function getUser(user){
-    const resp = await fetch(APIURL + user);
-    const data = resp.json();
-    console.log(data);
+async function getUser(username){
+    const resp = await fetch(APIURL + username);
+    const data = await resp.json();
+    
+    // console.log(data);
 
     createUserCard(data);
+    getUserRepos(username);
+};
+
+
+async function getUserRepos(username) {
+    const res = await fetch(APIURL + username + "/repos");
+    const data = await res.json();
+
+    console.log(data);
+
+    addReposToCard(data);
 }
 
 
 function createUserCard(user) {
-    const card = document.createElement("div");
-    card.classList.add("card");
+   
+    const cardHTML = `
+        <div class = "card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt = "${user.name ? user.name : user.login}"/>
+            </div>
 
-    card.innerHTML = `
-        <div>
-            <img src="${user.avatar}" alt = "${user.name}"/>
-        </div>
+            <div class = "user-info">
+                <h2>${user.name ? user.name : user.login}</h2>
+                <p>${user.bio}</p>
 
-        <div>
-            <h2>${user.name}</h2>
-            <p>${user.bio}</p>
-
-            <ul>
-                <li>${user.followers}</li>
-                <li>${user.following}</li>
-                <li>${user.public_repos}</li>
+                <ul>
+                    <li><i class='bx bxs-user-plus' ></i></i> <strong>${user.followers}</strong> <strong>Followers</strong></li>
+                    <li><i class='bx bxs-user'></i> <strong>${user.following}</strong> <strong>Following</strong></li>
+                    <li><i class='bx bx-folder-open' ></i> <strong>${user.public_repos}</strong> <strong>Repos</strong></li>
                 </ul>
+                <div id = "repos">Repos: <br/><br/> </div>
+            </div>
         </div>
     `;
 
-    main.appendChild(card);
-}
+    main.innerHTML = cardHTML;
+};
+
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+
+    repos.sort((a, b) => b.stargazers_count - a.stargazers_count).slice(0, 10).forEach((repo) => {
+        const repoEl = document.createElement("a");
+        repoEl.classList.add('repo');
+
+        repoEl.href = repo.html_url;
+        repoEl.target = "_blank";
+        repoEl.innerText = repo.name;
+
+        reposEl.appendChild(repoEl);
+    });
+};
+
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user.length > 0) {
+        getUser(user);
+
+        search.value = "";
+    };
+});
+
+
